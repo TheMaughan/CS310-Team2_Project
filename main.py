1de122cf3818a36083967ee655893534d5d51145
Marshall Haymond <68209576+HaymondM@users.noreply.github.com>
2021-02-11 06:57:10 -0700

    Game is working

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import arcade
+import arcade #py -m venv venv
 import random
 import os
 import math
@@ -6,7 +6,7 @@ import time
 
 
 SPRITE_SCALING = 0.2
-TILE_SCALING = 0.4
+TILE_SCALING = 0.9
 
 SCREEN_WIDTH = 750
 SCREEN_HEIGHT = 800
@@ -14,10 +14,10 @@ SCREEN_TITLE = "Platformer"
 
 
 MOVEMENT_SPEED = 3
-GRAVITY = 0 #change this to enable jumping
+GRAVITY = .09 #change this to enable jumping
 ANGLE_SPEED = 3
 
-class Car(arcade.Sprite):
+class Player(arcade.Sprite):
     """ Player Class """
 
     def __init__(self, image, scale):
@@ -66,15 +66,15 @@ class MyGame(arcade.Window):
         # Call the parent class initializer
         super().__init__(width, height, title)
 
-        self.player_list = None
-
+        
         self.physics_engine = None
 
         # Variables that will hold sprite lists
         self.player_list = None
-
+        self.platform_list =None
         # Set up the player info
         self.player_sprite = None
+        self.platform_sprite = None
 
         self.total_time = 90.0
 
@@ -87,60 +87,66 @@ class MyGame(arcade.Window):
 
     def setup(self): #change this section 
         """ Set up the game and initialize the variables. """
-
-
         
         # Create the Sprite lists
         self.player_list = arcade.SpriteList()
-	    self.platform = arcade.SpriteList()
+        self.platform_list = arcade.SpriteList()
 
         # Set up the player Change this
-        self.player_sprite = Car("car1.png", SPRITE_SCALING)
+        self.player_sprite = Player("Sprites\player.png", SPRITE_SCALING)
         self.player_sprite.center_x = 100
         self.player_sprite.center_y = 150
         self.player_list.append(self.player_sprite)
 
             # --- Load in a map from the tiled editor ---
-	"""
+
+	 
+
+        platform = arcade.Sprite("Sprites\platform.png", TILE_SCALING)
+        platform.center_x = 120
+        platform.center_y = 150
+        self.platform_list.append(platform)
+
+        platform = arcade.Sprite("Sprites\platform.png", TILE_SCALING)
+        platform.center_x = 250
+        platform.center_y = 150
+        self.platform_list.append(platform)
+
+        platform = arcade.Sprite("Sprites\platform.png", TILE_SCALING)
+        platform.center_x = 400
+        platform.center_y = 150
+        self.platform_list.append(platform)
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
+                                                             self.platform_list,
+                                                            GRAVITY)
+        
+        
+
+        """
         # Name of map file to load
         map_name = "background.tmx"
         # Name of the layer in the file that has our platforms/walls
         tree_name = 'Tree_plat'
         # Name of the layer that has items for pick-up
         coins_layer_name = 'Coins_plat'
-
         road_layer_name = 'Road_plat'
-
         house_layer_name = 'House_plat'
-
         # Read in the tiled map
         my_map = arcade.tilemap.read_tmx(map_name)
-
         # -- Platforms
         self.tree_list = arcade.tilemap.process_layer(my_map,
                                                       layer_name=tree_name,
                                                       scaling=TILE_SCALING,
                                                       use_spatial_hash=True)
-
-
         # --- Other stuff
         self.road_list = arcade.tilemap.process_layer(my_map,road_layer_name, TILE_SCALING )
-
         self.house_list = arcade.tilemap.process_layer(my_map,house_layer_name, TILE_SCALING )
-
-
         # Set the background color
         if my_map.background_color:
             arcade.set_background_color(my_map.background_color)
-	"""
-        # Create the 'physics engine'
-        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
-                                                             self.platform,
-                                                             GRAVITY)
-        
-        
-
-
+        """
 
 
 
@@ -157,13 +163,13 @@ class MyGame(arcade.Window):
 
         output = f"Time:  {minutes: 02d}:{seconds:02d}"
 
-        output2 = f"Total coins: {self.total_coins} "
+        #output2 = f"Total coins: {self.total_coins} "
 
-        won_text = "You got all the coins!"
+        #won_text = "You got all the coins!"
 
-        game_over = "Game Over"
+        #game_over = "Game Over"
 
-        total_score = f" Score: {self.total_coins}"
+        #total_score = f" Score: {self.total_coins}"
 
 
         
@@ -173,18 +179,19 @@ class MyGame(arcade.Window):
 
         # Draw all the sprites. Make sure you place background frist
         self.player_list.draw()
+        self.platform_list.draw()
 
 	
 	#take this part ot. Draws text on screen
         arcade.draw_text(output, 625, 750, arcade.color.BLACK, 18)
-        arcade.draw_text(output2, 610, 725, arcade.color.BLACK, 18)
+        #arcade.draw_text(output2, 610, 725, arcade.color.BLACK, 18)
 
-        if self.total_time < 0 :
-            arcade.draw_text(game_over, 75, 600, arcade.color.RED, 80) 
-            arcade.draw_text(total_score, 75, 520, arcade.color.RED, 80) 
-        if self.total_coins == 19:
-                arcade.draw_text(won_text, 75, 520, arcade.color.RED, 40)
-                arcade.draw_text(won_text, 75, 520, arcade.color.GLITTER, 41)
+        #if self.total_time < 0 :
+        #    arcade.draw_text(game_over, 75, 600, arcade.color.RED, 80) 
+        #    arcade.draw_text(total_score, 75, 520, arcade.color.RED, 80) 
+        #if self.total_coins == 19:
+        #        arcade.draw_text(won_text, 75, 520, arcade.color.RED, 40)
+        #        arcade.draw_text(won_text, 75, 520, arcade.color.GLITTER, 41)
 
 
 
@@ -200,57 +207,48 @@ class MyGame(arcade.Window):
 
         self.physics_engine.update()
 
-        self.player_list.update()
+
         #coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite,self.coin_list
-                                                             
-	"""
+        self.total_time -= delta_time                                       
+        """
         if self.total_time < 0:
             arcade.set_background_color(arcade.color.BLACK) 
         else:
             self.total_time -= delta_time
-
         if self.total_coins == 19:
             arcade.set_background_color(arcade.color.BLACK)
         else:
-            self.total_time -= delta_time
-	"""
+            
+	    """
 
         """
         In my code I reviewed, the scrolling was done in on_update
         This is the code directly from the tutorial I found:
-
         #This is the global variable used to determine when to screen scroll. It's in pixels.
         VIEWPORT_MARGIN = 250
-
         # Keep track of if we changed the boundary. We don't want to call the
         # set_viewport command if we didn't change the view port.
         changed = False
-
         # Scroll left
         left_boundary = self.view_left + VIEWPORT_MARGIN
         if self.player_sprite.left < left_boundary:
             self.view_left -= left_boundary - self.player_sprite.left
             changed = True
-
         # Scroll right
         right_boundary = self.view_left + SCREEN_WIDTH - VIEWPORT_MARGIN
         if self.player_sprite.right > right_boundary:
             self.view_left += self.player_sprite.right - right_boundary
             changed = True
-
         # Scroll up
         top_boundary = self.view_bottom + SCREEN_HEIGHT - VIEWPORT_MARGIN
         if self.player_sprite.top > top_boundary:
             self.view_bottom += self.player_sprite.top - top_boundary
             changed = True
-
         # Scroll down
         bottom_boundary = self.view_bottom + VIEWPORT_MARGIN
         if self.player_sprite.bottom < bottom_boundary:
             self.view_bottom -= bottom_boundary - self.player_sprite.bottom
             changed = True
-
-
         I think we might have some difficulty with making this to fit to a certain sized platform area. 
         In the example I added a box around the maze area to limit where the player can go.
         """
