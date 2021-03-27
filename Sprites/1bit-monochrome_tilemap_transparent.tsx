f263cea27809a9711a0165853313c83af571684b
Bryce <themaughan1.0@gmail.com>
2021-03-26 21:48:32 -0600

    Second_level

---
+++ Sprites/1bit-monochrome_tilemap_transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="monochrome_tilemap_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="400" columns="20">
+ <image source="../TilePacks/1-bit platformer pack/Tilemap/monochrome_tilemap_transparent.png" width="339" height="339"/>
+</tileset>
