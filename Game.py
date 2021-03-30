6cc1aa6c8c190189c84d5d6f448eb70e54df4549
Marshall Haymond <68209576+HaymondM@users.noreply.github.com>
2021-03-29 19:02:10 -0600

    Uncommented code

--- Game.py
+++ Game.py
@@ -370,16 +370,16 @@ class MyGame(arcade.View):
             enemy.remove_from_sprite_lists()
             question = self.interacion[len(self.enemy_hit_list)-1].get_question()
             answer = self.interacion[len(self.enemy_hit_list)-1].get_answer()
-            #firstnum = self.interacion[len(self.enemy_hit_list)-1].get_first()
-            #secondnum = self.interacion[len(self.enemy_hit_list)-1].get_second()
+            firstnum = self.interacion[len(self.enemy_hit_list)-1].get_first()
+            secondnum = self.interacion[len(self.enemy_hit_list)-1].get_second()
             #arcade.draw_point(self.player_sprite.center_x, self.player_sprite.center_y +100, arcade.color.BLACK, 18)
             arcade.draw_text(question, self.player_sprite.center_x , self.player_sprite.center_y + 100, arcade.color.BLACK, 18)
             arcade.draw_text(str(answer), self.player_sprite.center_x , self.player_sprite.center_y + 200, arcade.color.BLACK, 18)
             pause = PauseView(self)
             pause.set_question = question
             pause.set_answer = answer
-            #pause.set_first = firstnum
-            #pause.set_second = secondnum
+            pause.set_first = firstnum
+            pause.set_second = secondnum
             self.window.show_view(pause)
             #To be implimented once we fix the loop.
 
