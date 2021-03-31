b758dbc1d6b8901fc83881e0c9a51f1220b26125
kinofmat <72954040+kinofmat@users.noreply.github.com>
2021-03-31 13:16:54 -0600

    Fixed / 0 display error

--- Problem_Obj.py
+++ Problem_Obj.py
@@ -14,19 +14,20 @@ class Problems():
         self.fourth_val = random.randrange(7)
         self.question = ""
         self.answer = 0
-        self.question = "{} {} {}".format(self.first_val, self.MATH_LIST[self.operator], self.second_val)
         #After the initial declaration of variables this will generate the numerical value for the answer.
         if self.operator == 0:
             self.answer = self.first_val * self.second_val
         elif self.operator == 1:
             while self.second_val == 0: # Cannot divide by 0, generate a new random number.
-                self.second_val = random.randrange(9)
+                self.second_val = random.randrange(1,9)
             self.answer = self.first_val / self.second_val
         elif self.operator == 2:
             self.answer = self.first_val + self.second_val
         elif self.operator == 3:
             self.answer = self.first_val - self.second_val
         self.answer = round(self.answer, 3)
+        # Since we may be changing what the second value is so that we do not divide by zero it is necessary to assign the value for the question here.
+        self.question = "{} {} {}".format(self.first_val, self.MATH_LIST[self.operator], self.second_val)
 
     # Getters for another class to be able to use what was made in here.
     def get_question(self):
