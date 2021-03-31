d163f731b3c09a0c3d08f08b805e551e043d158b
kinofmat <72954040+kinofmat@users.noreply.github.com>
2021-03-31 14:55:03 -0600

    Menu music

--- Start_Game.py
+++ Start_Game.py
@@ -1,4 +1,5 @@
 import arcade
+import time
 
 # --- Constants ---
 SPRITE_SCALING_PLAYER = 0.5
@@ -8,6 +9,8 @@ COIN_COUNT = 50
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Implement Views Example"
+
+MUSIC_VOLUME = .1
 
 
 class InstructionView(arcade.View):
@@ -21,12 +24,32 @@ class InstructionView(arcade.View):
         self.button_states = {"new game": False, "continue": False}
 
         self.mouse_pos = (0,0)
+        self.current_player = None
+        self.music = None
+    
+
+    def play_song(self):
+        # Stop what is currently playing. This is to avoid having mutliple songs playing at the same time.
+        if self.music:
+            # This will keep there from just being silence playing and delete the player to keep there from wasted memory usage.
+            self.current_player.pause()
+            self.current_player.delete()
+
+        """From https://www.fesliyanstudios.com/royalty-free-music/downloads-c/8-bit-music/6
+        Another Website: https://freemusicarchive.org/genre/Chiptune"""
+        # Basically sets up the directory to the music.
+        self.music = arcade.Sound("Sprites\Old_Game_David_Fesliyan.mp3", streaming=True) # Streaming will tell the program to stream instead of making a copy of the file.
+        # Creates the music player, using the stored information from music. 
+        self.current_player = self.music.play(MUSIC_VOLUME)
+        # Gives a quick pause in the music. If we did not include this it would see the song as being done and skip through it.
+        time.sleep(0.03)
 
 
     def on_show(self):
         arcade.set_background_color(arcade.csscolor.DARK_SLATE_BLUE)
 
         arcade.set_viewport(0, SCREEN_WIDTH - 1, 0, SCREEN_HEIGHT - 1)
+        self.play_song()
 
     def on_draw(self):
         """ Draw this view """
@@ -47,6 +70,8 @@ class InstructionView(arcade.View):
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         """ If the user presses the mouse button, start the game. """
         if self.button_states["new game"] == True:
+            self.current_player.pause()
+            self.current_player.delete()
             print("creation of the NEW game..")
             from Game import MyGame
             game_view = MyGame()
@@ -54,6 +79,8 @@ class InstructionView(arcade.View):
             self.window.show_view(game_view)
 
         if self.button_states["continue"] == True:
+            self.current_player.pause()
+            self.current_player.delete()
             print("continue the game..")
             from Game import MyGame
             game_view = MyGame()
