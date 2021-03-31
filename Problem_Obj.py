17bcf52ae89ba913baa26516556700576f93db45
kinofmat <72954040+kinofmat@users.noreply.github.com>
2021-03-31 08:13:00 -0600

    Documentation

--- Problem_Obj.py
+++ Problem_Obj.py
@@ -9,16 +9,17 @@ class Problems():
         self.operator = random.randrange(4)
         self.first_val = random.randrange(9)
         self.second_val = random.randrange(9)
+        #These are extra values that are not necessary, but may be helpful for generating multiple choice answers.
         self.third_val = random.randrange(8)
         self.fourth_val = random.randrange(7)
         self.question = ""
         self.answer = 0
         self.question = "{} {} {}".format(self.first_val, self.MATH_LIST[self.operator], self.second_val)
-        #I want to take out of init so that this gneral format could still be used but adapted for making levels harder.
+        #After the initial declaration of variables this will generate the numerical value for the answer.
         if self.operator == 0:
             self.answer = self.first_val * self.second_val
         elif self.operator == 1:
-            while self.second_val == 0:
+            while self.second_val == 0: # Cannot divide by 0, generate a new random number.
                 self.second_val = random.randrange(9)
             self.answer = self.first_val / self.second_val
         elif self.operator == 2:
@@ -26,8 +27,8 @@ class Problems():
         elif self.operator == 3:
             self.answer = self.first_val - self.second_val
         self.answer = round(self.answer, 3)
-        #print("question is: " + str(self.question) + " answer is: " + str(self.answer))
 
+    # Getters for another class to be able to use what was made in here.
     def get_question(self):
         return self.question
 
--- README.md
+++ README.md
@@ -8,6 +8,8 @@ This repository contains 3 projects:
 
 [I-Learn Project Description](https://byui-cse.github.io/cse310-course/modules/team_project_description.html)
 
+We wanted to design a software to help younger kids learn math and get excited about it. We decided on making a game where the interaction between the player and enemy is based around math problems that the user has to solve. In order to run this, from our source code, you will use the file titled, "Start_Game.py" and run it. Alternatively we also created an executable file that will run the game as well.
+
 -----------------------------------------------------------------
 
 This is the README.md file to be edited at a future date...
@@ -21,8 +23,20 @@ This is the README.md file to be edited at a future date...
 * Visual Studio Code
 * Computer Whispering
 * Git Bash/GitHub
+* Arcade library for Python
+
+# Collaborators
+
+Bryce Maughan, Kai Matkin, Marshall Haymond, and Michael Campos
 
 # Useful Websites
 
 * [Robot Lives Matter](https://www.youtube.com/watch?v=uXcatFp3REg)
 * [Educational Video...](https://www.youtube.com/watch?v=igPIk79wAPg&t=1s)
+* [Arcade Examples](https://arcade.academy/examples/index.html#sprites)
+
+# Future Work
+
+* Impliment more levels and changing difficulty
+* Better sprite and game map handling
+* Item 3
