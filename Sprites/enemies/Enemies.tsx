8615fa08962ee18338b5d96d7f83e454ec5be32e
Bryce <themaughan1.0@gmail.com>
2021-03-31 20:34:11 -0600

    Getting the enemies ready, and making a 2nd player

---
+++ Sprites/enemies/Enemies.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Enemies" tilewidth="16" tileheight="16" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="8" source="goomba/gw2.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="goomba/g-idle.png"/>
+  <animation>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="goomba/gw1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="spike_turtle/3.png"/>
+  <animation>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="6" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <image width="14" height="16" source="spike_turtle/0.png"/>
+ </tile>
+ <tile id="5">
+  <image width="14" height="16" source="spike_turtle/1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="15" source="spike_turtle/2.png"/>
+ </tile>
+</tileset>
